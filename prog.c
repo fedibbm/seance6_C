12a3f94cba54671f868f59823dbb3dfe0f1a6a8a
fedibbm <benbrahimfedi4@gmail.com>
2024-10-18 14:06:53 +0100

    added c program

---
+++ prog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	int var, *ptr;
+	var = 2024;
+	ptr = &var;
+	
+
+	printf("%d %d %p %p \n",var, *ptr, &var, ptr);
+	var -= 2;
+ 	*ptr += 3;
+	printf("%d %d %d %p\n",var,*ptr,*&var,&*ptr);
+	
+	return 0;
+}
